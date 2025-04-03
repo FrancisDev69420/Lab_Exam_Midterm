27e77d641c73ffebda0f1208797c2769635a1c20
jkRi0 <rioverosjustine92@gmail.com>
2025-04-03 19:45:35 +0800

    modified the checkout monitoring controller

--- app/Http/Controllers/CheckoutMonitoringController.php
+++ app/Http/Controllers/CheckoutMonitoringController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Checkout;
+use Illuminate\Support\Facades\Auth;
 
 class CheckoutMonitoringController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json(Checkout::all(), 200);
+    }
+
+    public function show($id)
+    {
+        $checkout = Checkout::find($id);
+        if (!$checkout) {
+            return response()->json(['message' => 'Checkout not found'], 404);
+        }
+        return response()->json($checkout, 200);
+    }
+
+    public function filterByDate($date)
+    {
+        $checkouts = Checkout::whereDate('created_at', $date)->get();
+        return response()->json($checkouts, 200);
+    }
 }
